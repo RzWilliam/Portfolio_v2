1093ccea1318e3495cdc7d54cc377a3be3e78352
RzWilliam <90561147+RzWilliam@users.noreply.github.com>
2022-01-15 16:00:30 +0100

    :rocket: Add burger

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ boxes.forEach(function(box){
         box.classList.remove('reverse')
     })
 })
+
+let open_burger = document.querySelector('.burger');
+let container_burger = document.querySelector('.container_burger')
+
+open_burger.addEventListener('click', e=>{
+    if(open_burger.classList.contains('active')){
+        open_burger.classList.remove('active')
+        container_burger.classList.remove('active')
+    } else{
+        open_burger.classList.add('active')
+        container_burger.classList.add('active');
+    }
+})
